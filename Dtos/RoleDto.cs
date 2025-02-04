fe663e679bb8433dd67c8fbf83179a8ca1784bb6
AngelPelayo <pelayo.ajpo@gmail.com>
2025-02-03 22:09:18 -0700

    ya

--- Dtos/RoleDto.cs
+++ Dtos/RoleDto.cs
@@ -4,7 +4,6 @@ namespace CRM_ERP_UNID.Dtos;
 
 public class RoleDto
 {
-    [Column("RoleId")]
     public Guid RoleId { get; set; }
     public string RoleName { get; set; }
     public List<RolePermissionDto>? RolePermissions { get; set; }
